87e2f9a391ca7218ac8e95528002aeb87165a1de
Gaurangjoshi47 <gaurangjoshi599@gmail.com>
2025-07-20 15:46:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/travel-website/', // 👈 Use your actual repo name here
+  base: '/travel-now/', // 👈 Use your actual repo name here
   plugins: [react()],
 })
